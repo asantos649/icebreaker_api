229006684c19e6d1a1a31c0a7d3190e488beffc0
asantos649 <asantos@alumni.nd.edu>
2020-08-11 14:31:20 -0400

    added question index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create({text:'What is your perfect breakfast?'})
+Question.create({text:'If you could have any exotic animal as a pet, which would you choose?'})
+Question.create({text:'If you were a kitchen appliance or utensil, which would you be?'})
+Question.create({text:'If you were a candy, which would you be?'})
+Question.create({text:'If you could marry a food, which would you want to marry?'})
+Question.create({text:'If you could add any competition to the Olympics, what would you add so you can win a Gold Medal?'})
+Question.create({text:'If you had a single use of a time machine, when would you travel to spend the rest of your life?'})
