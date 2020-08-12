c73145cb0be3fce36ff12f314c4cb96421c5bf38
asantos649 <asantos@alumni.nd.edu>
2020-08-12 17:45:15 -0400

    removed some keys from index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
 
     def index
         questions = Question.all
-        render json: questions
+        render json: questions, except: [:created_at, :updated_at]
     end
 
 end
